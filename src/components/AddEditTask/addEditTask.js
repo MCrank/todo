fbc4480e3a21f6c5a01c13591f43f5cc4f0eb398
MCrank <marco_mcse@hotmail.com>
2018-11-26 01:29:47 -0600

    refactor add-edit to new component

---
+++ src/components/AddEditTask/addEditTask.js
@@ -0,0 +1,33 @@
+import $ from 'jquery';
+import 'bootstrap';
+import './addEditTask.scss';
+
+const addTaskBuilder = () => {
+  const newTaskInput = `
+    <form>
+      <div class="form-group w-100">
+        <label for="newTaskInput">Task Name</label>
+        <input type="text" class="form-control w-100" id="newTaskInput" placeholder="Enter new task name" autofocus/>
+      </div>
+    </form>
+    `;
+  $('#new-task-input').html(newTaskInput);
+};
+
+const editTaskBuilder = (evt) => {
+  const editTaskName = $(evt.target)
+    .closest('tr')
+    .find('th')[0].innerHTML;
+  console.log(editTaskName);
+  const editTaskInput = `
+    <form>
+      <div class="form-group w-100">
+        <label for="editTaskInput">Task Name</label>
+        <input type="text" class="form-control w-100" id="editTaskInput" value="${editTaskName}" autofocus/>
+      </div>
+    </form>
+    `;
+  $('#new-task-input').html(editTaskInput);
+};
+
+export default { addTaskBuilder, editTaskBuilder };
