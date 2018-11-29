d77a4c5303349df5c5dea5a582ed90a95453d7b3
MCrank <marco_mcse@hotmail.com>
2018-11-28 18:24:12 -0600

    new login page

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -4,11 +4,23 @@ import 'firebase/auth';
 
 import './auth.scss';
 
+const loginPage = () => {
+  const loginPageString = `
+  <div class="container d-flex justify-content-center">
+    <div class="card bg-light my-5" style="max-width: 16.8rem;">
+      <div class="card-body">
+        <a href="#" class="sb sb-google sb-m-2 sb-fw" id="google">Sign in with Google</a>
+      </div>
+    </div>
+  </div>
+  `;
+  $('#login-buttons').html(loginPageString);
+};
+
 const authEvents = () => {
-  $('#nav-login').on('click', () => {
+  $('body').on('click', '#google', () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider);
   });
 };
-
-export default { authEvents };
+export default { loginPage, authEvents };
--- src/components/helpers/authHelper.js
+++ src/components/helpers/authHelper.js
@@ -1,19 +1,19 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-
 import $ from 'jquery';
+import auth from '../Auth/auth';
 
 const checkLoginStatus = (initTaskPage) => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      $('#nav-login').hide();
+      $('#login-buttons').html('');
       $('#nav-logout').show();
       $('#nav-add-task').show();
       initTaskPage();
     } else {
-      $('#nav-login').show();
       $('#nav-logout').hide();
       $('#nav-add-task').hide();
+      auth.loginPage();
     }
   });
 };
